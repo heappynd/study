1e98ac71761139fdf8706c8cbb3f3778d8db3c93
heappynd <heappynd@163.com>
2022-07-13 13:35:48 +0800

    feat: 字典

--- data-structures/packages/set/index.ts
+++ data-structures/packages/set/index.ts
@@ -1,10 +1,10 @@
 export { MySet } from './my-set'
 
-const setA = new Set()
-const setB = new Set()
+// const setA = new Set()
+// const setB = new Set()
 
-console.log(new Set([...setA, ...setB]))
+// console.log(new Set([...setA, ...setB]))
 
-console.log(new Set([...setA].filter((x) => setB.has(x))))
+// console.log(new Set([...setA].filter((x) => setB.has(x))))
 
-console.log(new Set([...setA].filter((x) => !setB.has(x))))
+// console.log(new Set([...setA].filter((x) => !setB.has(x))))
