c7becbc96b2ef7bb0dfd7871f4713e93d7f43100
heappynd <heappynd@163.com>
2022-07-18 22:05:02 +0800

    feat: computed完善

--- source-code/src/computed.ts
+++ source-code/src/computed.ts
@@ -1,4 +1,4 @@
-import { effect } from './effect'
+import { effect, track, trigger } from './effect'
 
 export function computed(getter: () => any) {
   // 用来缓存上一次计算的值
@@ -10,7 +10,11 @@ export function computed(getter: () => any) {
   const effectFn = effect(getter, {
     lazy: true,
     scheduler() {
-      dirty = true
+      if (!dirty) {
+        dirty = true
+        // 当计算属性依赖的响应式数据变化时 手动调用trigger函数触发响应
+        trigger(obj, 'value')
+      }
     },
   })
 
@@ -21,6 +25,8 @@ export function computed(getter: () => any) {
         value = effectFn()
         dirty = false
       }
+      // 当读取value时，手动调用track函数进行追踪
+      track(obj, 'value')
       return value
     },
   }
