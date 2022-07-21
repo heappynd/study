18e77723b1f9d5bf0241aa02c6efca4f68269082
heappynd <heappynd@163.com>
2022-07-21 23:32:18 +0800

    feat: 如何代理object

--- source-code/src/main.ts
+++ source-code/src/main.ts
@@ -2,20 +2,18 @@ import { effect } from './effect'
 import { reactive } from './reactive'
 import { watch } from './watch'
 
-type Target = Record<'foo' | 'bar', number>
+type Target = Record<'foo', number>
 
 const proxy: Target = reactive({
   foo: 10,
-
-  get bar() {
-    return this.foo
-  },
 })
 
 effect(() => {
-  console.log(proxy.bar)
+  for (const key in proxy) {
+    console.log(key)
+  }
 })
 
 setTimeout(() => {
-  proxy.foo = 20
+  proxy.bar = 200
 }, 1000)
