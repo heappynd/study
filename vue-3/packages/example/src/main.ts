16d9cd5b3d1bec85be05eed0e2eccf9512f3c24d
heappynd <heappynd@163.com>
2022-08-12 23:18:31 +0800

    feat(compiler): parse 解析器正则版本

--- vue-3/packages/example/src/main.ts
+++ vue-3/packages/example/src/main.ts
@@ -1,5 +1,8 @@
 import { compiler } from '@vue-3/compiler'
+import { parse as sParse } from '@vue-3/compiler/src/s-parse'
 
-const template = `<div><p>Vue</p><p>Template</p></div>`
+const template = `<div>Vu&gt;e</div>`
 
-console.log(compiler(template))
+// console.log(compiler(template))
+
+console.log(sParse(template))
