d9cde2c9d54e5b0c719bbd134e6f93b662ad5966
heappynd <heappynd@163.com>
2022-08-19 00:28:16 +0800

    feat(ts): function

--- ts-demo/src/index.ts
+++ ts-demo/src/index.ts
@@ -1,11 +1,34 @@
-function sumVariadic(...args: number[]): number {
-  return args.reduce((total, n) => total + n, 0)
+function sum(a: number, b: number): number {
+  return a + b
 }
 
-sumVariadic(1, 2, 3)
-
-function fancyDate(this: Date) {
-  return this.getDate()
+function times(f: (index: number) => void, n: number) {
+  for (let i = 0; i < n; i++) {
+    f(i)
+  }
 }
 
-fancyDate.call(new Date())
+times((n) => {
+  console.log(n.toFixed(2))
+}, 3)
+
+type Reverse = {
+  (from: Date, to: Date, destination: string): void
+  (from: Date, destination: string): void
+}
+
+let reverse: Reverse = (
+  from: Date,
+  to: Date | string,
+  destination?: string
+) => {}
+
+function map<T, U>(array: T[], f: (item: T) => U): U[] {
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    result.push(f(array[i]))
+  }
+  return result
+}
+let m = [1, 2, 3]
+map(m, (item) => item.toFixed(2))
