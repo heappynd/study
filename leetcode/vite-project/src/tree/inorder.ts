be221c72ffdefdc3ee32709f151846f53142249e
heappynd <heappynd@163.com>
2022-08-30 23:47:19 +0800

    feat(tree): 二叉树的先中后序遍历（非递归版）

--- leetcode/vite-project/src/tree/inorder.ts
+++ leetcode/vite-project/src/tree/inorder.ts
@@ -4,9 +4,21 @@ function inorder(root: BTree | null) {
   if (!root) {
     return
   }
-  inorder(root.left)
-  console.log(root.val)
-  inorder(root.right)
+  // inorder(root.left)
+  // console.log(root.val)
+  // inorder(root.right)
+  const stack = []
+  let p = root
+  debugger
+  while (stack.length || p) {
+    while (p) {
+      stack.push(p)
+      p = p.left
+    }
+    const n = stack.pop()!
+    console.log(n.val)
+    p = n.right
+  }
 }
 console.log('---')
 
--- leetcode/vite-project/src/tree/preorder.ts
+++ leetcode/vite-project/src/tree/preorder.ts
@@ -4,9 +4,22 @@ function preorder(root: BTree | null) {
   if (!root) {
     return
   }
-  console.log(root.val)
-  preorder(root.left)
-  preorder(root.right)
+  // console.log(root.val)
+  // preorder(root.left)
+  // preorder(root.right)
+
+  const stack = [root]
+  debugger
+  while (stack.length) {
+    const n = stack.pop()!
+    console.log(n.val)
+    if (n.right) {
+      stack.push(n.right)
+    }
+    if (n.left) {
+      stack.push(n.left)
+    }
+  }
 }
 
 console.log('----')
