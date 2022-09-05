5efe52a01ddd6296860dc3e1d5e3b169ef6c9b80
heappynd <heappynd@163.com>
2022-09-05 22:02:42 +0800

    feat: useReducer

---
+++ react-demo/src/Reducer.jsx
@@ -0,0 +1,27 @@
+import React, { useReducer } from 'react'
+
+const initialState = {
+  count: 0,
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
+
+export default function Reducer() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+    </div>
+  )
+}
