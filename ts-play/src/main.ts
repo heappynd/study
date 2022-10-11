2cb8672eeceb1691d4f6b0e2f9453e4cda2f705a
heappynd <heappynd@163.com>
2022-10-12 00:10:21 +0800

    feat: everyday-types

--- ts-play/src/main.ts
+++ ts-play/src/main.ts
@@ -4,5 +4,20 @@ function greet(person: string, date: Date) {
 
 greet('Maddison', new Date())
 
-// let num: number = null
+let num = null // any
 // let str: string = undefined
+
+// useful "as const"
+declare function handleRequest(url: string, method: 'GET' | 'POST'): void
+// const req = { url: 'https://example.com', method: 'GET' as 'GET' }
+const req = { url: 'https://example.com', method: 'GET' } as const
+handleRequest(req.url, req.method)
+// handleRequest(req.url, req.method as 'GET')
+
+// Creating a bigint via the BigInt function
+const oneHundred: bigint = BigInt(100)
+// Creating a BigInt via the literal syntax
+const anotherHundred: bigint = 100n
+
+const firstName = Symbol('name')
+const secondName = Symbol('name')
