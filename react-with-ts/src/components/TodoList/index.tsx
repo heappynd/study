90e85e92b6a5bffab83d91bdd13501fe1c64f97b
heappynd <heappynd@163.com>
2022-10-19 22:17:37 +0800

    feat: useReducer

--- react-with-ts/src/components/TodoList/index.tsx
+++ react-with-ts/src/components/TodoList/index.tsx
@@ -3,28 +3,35 @@ import React, {
   ReactElement,
   useCallback,
   useEffect,
+  useReducer,
   useState,
 } from 'react'
 import TdInput from './Input'
 import TdList from './List'
-import { ITodo } from './typings'
+import { todoReducer } from './reducer'
+import { ACTION_TYPE, IState, ITodo } from './typings'
+
+const initialState: IState = {
+  todoList: [],
+}
 
 const TodoList: FC = (): ReactElement => {
-  const [todoList, setTodoList] = useState<ITodo[]>([])
+  // const [todoList, setTodoList] = useState<ITodo[]>([])
+  const [state, dispatch] = useReducer(todoReducer, initialState)
 
   useEffect(() => {
-    console.log(todoList)
-  }, [todoList])
+    console.log(state.todoList)
+  }, [state.todoList])
 
   const addTodo = useCallback((todo: ITodo) => {
     console.log(todo)
-
-    setTodoList((todoList) => [...todoList, todo])
+    // setTodoList((todoList) => [...todoList, todo])
+    dispatch({ type: ACTION_TYPE.ADD_TODO, payload: todo })
   }, [])
 
   return (
     <div>
-      <TdInput addTodo={addTodo} todoList={todoList} />
+      <TdInput addTodo={addTodo} todoList={state.todoList} />
       <TdList />
     </div>
   )
