4e270dcc0915a280ed30ae34a7af555cf39d011b
Lov`u`e <105651386+heappynd@users.noreply.github.com>
2022-11-30 15:09:43 +0000

    feat: eslint

--- eslint-demo/yourfile.js
+++ eslint-demo/yourfile.js
@@ -1,2 +1,10 @@
-const a = 123
-// const a = 123
+for (let i = 0; i < 10; i++) {}
+
+let arr = [1, 2, 3]
+let obj = { a: 1, b: 2, c: 3 }
+
+let e = arr[0]
+/* eslint prefer-destructuring: off */
+
+let b = 1
+let a = obj.a
