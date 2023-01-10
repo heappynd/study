910612475c81bf6c542470b55480903f23470e85
heappynd <heappynd@163.com>
2023-01-10 23:31:29 +0800

    feat: tailwindcss

---
+++ tailwindcss-demo/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        "4/3": "4 / 3",
+      },
+    },
+    // container: {
+    //   padding: {},
+    // },
+  },
+  plugins: [],
+};
