31f2a3a1c9601985101c8d6222b6fa2ff0a61a12
heappynd <heappynd@163.com>
2023-01-18 11:53:30 +0800

    feat: svelte

---
+++ svelte-demo/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0
+  $: doubled = count * 2
+  function handleClick() {
+    count++
+  }
+  $: {
+    console.log(`the count is ${count}`)
+  }
+  $: if (count >= 10) {
+    alert(`count is dangerously high!`)
+    count = 9
+  }
+</script>
+
+<main>
+  <h1>{count}</h1>
+  <h2>{doubled}</h2>
+  <button on:click={handleClick}>+</button>
+</main>
+
+<style>
+</style>
