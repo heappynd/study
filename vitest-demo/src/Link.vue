3081e9df2fc127960dedf178794ed2e7ed8773c0
heappynd <heappynd@163.com>
2023-02-20 19:00:02 +0800

    feat: vitest

---
+++ vitest-demo/src/Link.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2 :style="{ color: color }">Link</h2>
+</template>
+
+<script setup>
+const props = defineProps({
+  color: {
+    type: String,
+    default: "red",
+  },
+});
+</script>
---
+++ vitest-demo/src/sum.test.ts
@@ -0,0 +1,7 @@
+import sum from "./sum";
+import { describe, expect, it } from "vitest";
+describe("#sum", () => {
+  it("returns 0 with no numbers", () => {
+    expect(sum()).toBe(0);
+  });
+});
