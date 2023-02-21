0da687d81ad8635d45392d8d8eb46bcad81e4704
heappynd <heappynd@163.com>
2023-02-21 13:44:12 +0800

    feat(vue):  分支切换与 cleanup

--- mini-vue/src/main.ts
+++ mini-vue/src/main.ts
@@ -1,12 +1,30 @@
 const bucket = new WeakMap();
 
-const data = { text: "hello world" };
+const data = { ok: true, text: "hello vue3" };
 
 let activeEffect;
-
+// 用一个全局变量存储被注册的副作用函数
 function effect(fn) {
-  activeEffect = fn;
-  fn();
+  const effectFn = () => {
+    // 调用 cleanup 函数完成清除工作
+    cleanup(effectFn);
+    // 当 effectFn 执行时，将其设置为当前激活的副作用函数
+    activeEffect = effectFn;
+    fn();
+  };
+  // activeEffect.deps 用来存储所有与该副作用函数相关联的依赖集合
+  effectFn.deps = [];
+  effectFn();
+}
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    // deps 是依赖集合
+    const deps = effectFn.deps[i];
+    // 将 effectFn 从依赖集合中移除
+    deps.delete(effectFn);
+  }
+  // 最后需要重置 effectFn.deps 数组
+  effectFn.deps.length = 0;
 }
 
 const obj = new Proxy(data, {
@@ -22,7 +40,6 @@ const obj = new Proxy(data, {
     console.log("- 拦截设置操作 newVal", newVal);
     target[key] = newVal;
     trigger(target, key);
-    return true
   },
 });
 
@@ -42,21 +59,28 @@ function track(target, key) {
     depsMap.set(key, (deps = new Set()));
   }
   deps.add(activeEffect);
+  // deps 就是一个与当前副作用函数存在联系的依赖集合
+  // 将其添加到 activeEffect.deps 数组中
+  activeEffect.deps.push(deps); // 新增
 }
 function trigger(target, key) {
   const depsMap = bucket.get(target);
   if (!depsMap) return;
   const effects = depsMap.get(key);
-  effects && effects.forEach((fn) => fn());
+  const effectsToTun = new Set(effects);
+  effectsToTun && effectsToTun.forEach((effectFn) => effectFn());
 }
 
 effect(() => {
-  console.log(bucket);
-  
   console.log("effect run");
-  document.body.innerText = obj.text;
+
+  document.body.innerText = obj.ok ? obj.text : "not";
 });
 
 setTimeout(() => {
-  obj.text2= "hello vue3";
+  obj.ok = false;
 }, 1000);
+
+setTimeout(() => {
+  obj.text = "hello world";
+}, 2000);
