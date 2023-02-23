4c430363cfa646ab3ab1da33baad78ca2a568d4a
heappynd <heappynd@163.com>
2023-02-23 14:16:38 +0800

    feat(vue): computed嵌套在effect中

--- mini-vue/src/reactivity/src/computed.ts
+++ mini-vue/src/reactivity/src/computed.ts
@@ -1,4 +1,4 @@
-import { effect } from "./effect";
+import { effect, track, trigger } from "./effect";
 
 export function computed(getter) {
   // value 用来缓存上一次计算的值
@@ -11,6 +11,8 @@ export function computed(getter) {
     // 添加调度器，在调度器中将 dirty 重置为 true
     scheduler() {
       dirty = true;
+      // 当计算属性依赖的响应式数据变化时，手动调用 trigger 函数触发响应
+      trigger(obj, "value");
     },
   });
   const obj = {
@@ -22,6 +24,8 @@ export function computed(getter) {
         // 将 dirty 设置为 false，下一次访问直接使用缓存到 value 中的值
         dirty = false;
       }
+      // 当读取 value 时，手动调用 track 函数进行追踪
+      track(obj, "value");
       return value;
     },
   };
