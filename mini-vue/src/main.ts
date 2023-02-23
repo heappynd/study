e72347e486a9737cb505ba046cd0b785eeac3552
heappynd <heappynd@163.com>
2023-02-23 14:11:18 +0800

    feat(vue): computed add cache

--- mini-vue/src/main.ts
+++ mini-vue/src/main.ts
@@ -1,36 +1,8 @@
-import { reactive, effect } from "./reactivity/src";
+import { reactive, effect, computed } from "./reactivity/src";
 
-const obj = reactive({ foo: 1 });
+const data = { foo: 1, bar: 2 };
+const obj = reactive(data);
 
-const jobQueue = new Set();
+const sumRes = computed(() => obj.foo + obj.bar);
 
-const p = Promise.resolve();
-
-let isFlushing = false;
-
-function flushJob() {
-  if (isFlushing) {
-    return;
-  }
-  isFlushing = true;
-  p.then(() => {
-    jobQueue.forEach((job) => job());
-  }).finally(() => {
-    isFlushing = false;
-  });
-}
-
-effect(
-  () => {
-    console.log(obj.foo);
-  },
-  {
-    scheduler(fn) {
-      jobQueue.add(fn);
-      flushJob();
-    },
-  }
-);
-
-obj.foo++;
-obj.foo++;
+console.log(sumRes.value);
