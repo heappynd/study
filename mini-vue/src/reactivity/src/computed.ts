a5d38d2c739f9478370735fab50110d5646c2aa5
heappynd <heappynd@163.com>
2023-02-23 14:12:52 +0800

    feat(vue): computed 响应式数据变化时执行

--- mini-vue/src/reactivity/src/computed.ts
+++ mini-vue/src/reactivity/src/computed.ts
@@ -8,6 +8,10 @@ export function computed(getter) {
   // 把 getter 作为副作用函数，创建一个 lazy 的 effect
   const effectFn = effect(getter, {
     lazy: true,
+    // 添加调度器，在调度器中将 dirty 重置为 true
+    scheduler() {
+      dirty = true;
+    },
   });
   const obj = {
     // 当读取 value 时才执行 effectFn
