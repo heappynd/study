d42549244b5d1ffec5a63be9451796636d1e2431
heappynd <heappynd@163.com>
2023-02-25 17:21:12 +0800

    feat(vue): 合理地触发响应

--- mini-vue/src/reactivity/__test__/reactive.test.ts
+++ mini-vue/src/reactivity/__test__/reactive.test.ts
@@ -51,4 +51,44 @@ describe('reactive/test', () => {
     delete obj.name
     expect(fn).toHaveBeenCalledTimes(3)
   })
+
+  it('数据不变不触发更新', () => {
+    let dummy
+    const obj = reactive({
+      age: 1,
+    })
+    const fn = vi.fn(() => {
+      dummy = obj.age
+    })
+    effect(fn)
+    obj.age = 1
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+
+  it('NaN', () => {
+    let dummy
+    const obj = reactive({
+      foo: NaN,
+    })
+    const fn = vi.fn(() => {
+      dummy = obj.foo
+    })
+    effect(fn)
+    obj.foo = NaN
+    expect(fn).toHaveBeenCalledTimes(1)
+  })
+
+  it('reactive inherited', () => {
+    let dummy
+    const child = reactive({})
+    const parent = reactive({ bar: 1 })
+    Object.setPrototypeOf(child, parent)
+    const fn = vi.fn(() => {
+      dummy = child.bar
+    })
+    effect(fn)
+    expect(dummy).toBe(1)
+    child.bar = 2
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
 })
--- mini-vue/src/reactivity/src/reactive.ts
+++ mini-vue/src/reactivity/src/reactive.ts
@@ -5,6 +5,10 @@ export function reactive(data) {
   return new Proxy(data, {
     get(target, key, receiver) {
       // console.log("拦截读取操作", key);
+      // 代理对象可以通过 raw 属性访问原始数据
+      if (key === 'raw') {
+        return target
+      }
 
       track(target, key)
 
@@ -12,12 +16,22 @@ export function reactive(data) {
     },
     set(target, key, newVal, receiver) {
       // console.log("- 拦截设置操作", key, newVal);
+      // 先获取旧值
+      const oldVal = target[key]
       // 如果属性不存在，则说明是在添加新属性，否则是设置已有属性
       const type = Object.prototype.hasOwnProperty.call(target, key)
         ? TriggerType.SET
         : TriggerType.ADD
       const res = Reflect.set(target, key, newVal, receiver)
-      trigger(target, key, type)
+      // target === receiver.raw 说明 receiver 就是 target 的代理对象
+      if (target === receiver.raw) {
+        // 比较新值与旧值，只要当不全等的时候才触发响应
+        // 比较新值与旧值，只有当它们不全等，并且不都是 NaN 的时候才触发响应
+        if (oldVal !== newVal && (oldVal === oldVal || newVal === newVal)) {
+          trigger(target, key, type)
+        }
+      }
+
       return res
     },
     has(target, p) {
