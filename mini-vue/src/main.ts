d47f335b1e4bf4a013ac9cb69f86c40e1488b37c
heappynd <heappynd@163.com>
2023-03-01 22:15:51 +0800

    feat: why Reflect

--- mini-vue/src/main.ts
+++ mini-vue/src/main.ts
@@ -2,13 +2,22 @@ import { reactive, effect, computed, watch } from './reactivity/src'
 
 const obj = {
   foo: 1,
+  get bar() {
+    console.log(this);
+    
+    return this.foo
+  }
 }
 
 const p = new Proxy(obj, {
-  has(target, p) {
-    console.log(target, p)
-    return Reflect.has(target, p)
+  get(target, p, receiver) {
+    console.log(target, p, receiver);
+    // return target[p]
+    return Reflect.get(target, p, receiver)
+  },
+  set(target, p, newValue, receiver) {
+    
   },
 })
 
-console.log('foo' in p)
+console.log(p.bar);
