8c0d91460307ee941e44e39f653fdfb70b01b584
heappynd <heappynd@163.com>
2023-03-08 15:48:33 +0800

    feat(vue): 事件绑定

--- mini-vue/src/main.ts
+++ mini-vue/src/main.ts
@@ -4,7 +4,12 @@ import { shouldSetAsProps } from './renderer/src/utils'
 // test
 const vnode1 = {
   type: 'p',
-  children: 'hello',
+  children: 'text',
+  props: {
+    onClick: () => {
+      alert('clicked')
+    },
+  },
 }
 
 // 在创建 renderer 时传入配置项
@@ -22,8 +27,16 @@ const renderer = createRenderer({
     parent.insertBefore(el, anchor)
   },
   // 将属性设置相关操作封装到 patchProps 函数中，并作为渲染器选项传递
-  patchProps(el, key, prevValue, nextValue) {
-    if (key === 'class') {
+  patchProps(el: Element, key, prevValue, nextValue) {
+    // 匹配以 on 开头的属性，视其为事件
+    if (/^on/.test(key)) {
+      // 根据属性名称得到对应的事件名称，例如 onClick ---> click
+      const name = key.slice(2).toLowerCase()
+      // 移除上一次绑定的事件处理函数
+      prevValue && el.removeEventListener(name, prevValue)
+      // 绑定事件，nextValue 为事件处理函数
+      el.addEventListener(name, nextValue)
+    } else if (key === 'class') {
       el.className = nextValue || ''
     } else if (shouldSetAsProps(el, key, nextValue)) {
       // 获取该 DOM Properties 的类型
@@ -44,12 +57,11 @@ const renderer = createRenderer({
 
 renderer.render(vnode1, document.querySelector('#app'))
 
-
 setTimeout(() => {
   const vnode2 = {
     type: 'input',
     children: 'world',
   }
-  renderer.render(vnode2, document.querySelector('#app'))
+  // renderer.render(vnode2, document.querySelector('#app'))
   // renderer.render(null, document.querySelector('#app'))
 }, 1000)
