e8cf5f9a65e22bc51d1dbd7622ebca089299d633
heappynd <heappynd@163.com>
2023-03-08 17:08:13 +0800

    fix(vue): 屏蔽所有绑定时间 晚于事件触发时间的事件处理函数的执行

--- mini-vue/src/main.ts
+++ mini-vue/src/main.ts
@@ -1,79 +1,11 @@
 import { createRenderer } from './renderer/src'
-import { shouldSetAsProps } from './renderer/src/utils'
 import { ref, effect } from './reactivity/src'
+import { options } from './renderer/src/dom'
 
 // test
 
 // 在创建 renderer 时传入配置项
-const renderer = createRenderer({
-  // 用于创建元素
-  createElement(tag) {
-    return document.createElement(tag)
-  },
-  // 用于设置元素的文本节点
-  setElementText(el, text) {
-    el.textContent = text
-  },
-  // 用于在给定的 parent 下添加指定元素
-  insert(el, parent, anchor = null) {
-    parent.insertBefore(el, anchor)
-  },
-  // 将属性设置相关操作封装到 patchProps 函数中，并作为渲染器选项传递
-  patchProps(el: Element, key, prevValue, nextValue) {
-    // 匹配以 on 开头的属性，视其为事件
-    if (/^on/.test(key)) {
-      // 获取为该元素伪造的事件处理函数 invoker
-      // 定义 el._vei 为一个对象，存在事件名称到事件处理函数的映射
-      const invokers = el._vei || (el._vei = {})
-      // 根据事件名称获取 invoker
-      let invoker = invokers[key]
-      // 根据属性名称得到对应的事件名称，例如 onClick ---> click
-      const name = key.slice(2).toLowerCase()
-      if (nextValue) {
-        if (!invoker) {
-          // 如果没有 invoker，则将一个伪造的 invoker 缓存到 el._vei 中
-          // vei 是 vue event invoker 的首字母缩写
-          // 将事件处理函数缓存到 el._vei[key] 下，避免覆盖
-          invoker = el._vei[key] = (e) => {
-            // 当伪造的事件处理函数执行时，会执行真正的事件处理函数
-            // 如果 invoker.value 是数组，则遍历它并逐个调用事件处理函数
-            if (Array.isArray(invoker.value)) {
-              invoker.value.forEach((fn) => fn(e))
-            } else {
-              // 否则直接作为函数调用
-              invoker.value(e)
-            }
-          }
-          // 将真正的事件处理函数赋值给 invoker.value
-          invoker.value = nextValue
-          // 绑定 invoker 作为事件处理函数
-          el.addEventListener(name, invoker)
-        } else {
-          // 如果 invoker 存在，意味着更新，并且只需要更新 invoker.value的值即可
-          invoker.value = nextValue
-        }
-      } else if (invoker) {
-        // 新的事件绑定函数不存在，且之前绑定的 invoker 存在，则移除绑定
-        el.removeEventListener(name, invoker)
-      }
-    } else if (key === 'class') {
-      el.className = nextValue || ''
-    } else if (shouldSetAsProps(el, key, nextValue)) {
-      // 获取该 DOM Properties 的类型
-      const type = typeof el[key]
-      if (type === 'boolean' && nextValue === '') {
-        // 如果是布尔类型，并且 value 是空字符串，则将值矫正为 true
-        el[key] = true
-      } else {
-        el[key] = nextValue
-      }
-    } else {
-      // 如果要设置的属性没有对应的 DOM Properties，
-      // 则使用setAttribute 函数设置属性
-      el.setAttribute(key, nextValue)
-    }
-  },
-})
+const renderer = createRenderer(options)
 
 const bol = ref(false)
 effect(() => {
