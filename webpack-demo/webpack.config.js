fc852bdc49e9050c1032b3e0a5e2d42e38c3aab1
heappynd <heappynd@163.com>
2023-12-05 22:47:40 +0800

    fix: 1205

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -5,42 +5,17 @@ const glob = require("glob");
 
 /** @type { import("webpack").Configuration } */
 const config = {
-  mode: "development",
-  entry: {},
+  mode: "production",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     // filename: "bundle.js",
   },
   devtool: false,
   module: {},
-  plugins: [],
+  plugins: [new HtmlWebpackPlugin()],
 };
 
 module.exports = () => {
-  const entryFiles = glob.sync("./src/**/index.js");
-  console.log(entryFiles);
-  const entrys = {};
-  const htmlWebpackPlugins = [];
-  entryFiles.forEach((file) => {
-    // TODO: only support windows
-    const res = file.match(/\\(.*)\\index\.js/);
-    console.log(res);
-    entrys[res[1]] = {
-      import: "./" + file,
-    };
-
-    htmlWebpackPlugins.push(
-      new HtmlWebpackPlugin({
-        filename: res[1] + ".html",
-        chunks: [res[1]],
-      })
-    );
-  });
-
-  config.entry = entrys;
-  console.log(entrys);
-  console.log(htmlWebpackPlugins);
-  config.plugins = htmlWebpackPlugins.concat(config.plugins);
-
   return config;
 };
