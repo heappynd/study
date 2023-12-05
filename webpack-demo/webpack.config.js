652e6918ad39dc8ab36c3077f510518fba54062e
heappynd <heappynd@163.com>
2023-12-05 21:49:38 +0800

    fix: 1205

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,51 +1,46 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const glob = require("glob");
 
 /** @type { import("webpack").Configuration } */
-module.exports = {
+const config = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: {},
   output: {
     path: path.resolve(__dirname, "dist"),
     // filename: "bundle.js",
   },
   devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    "autoprefixer",
-                    // {
-                    //   browser: ["ie > 8"],
-                    // },
-                  ],
-                ],
-              },
-            },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        type: "asset/source",
-        include: path.resolve(__dirname, "./src/inline"),
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+  module: {},
+  plugins: [],
+};
+
+module.exports = () => {
+  const entryFiles = glob.sync("./src/**/index.js");
+  console.log(entryFiles);
+  const entrys = {};
+  const htmlWebpackPlugins = [];
+  entryFiles.forEach((file) => {
+    // TODO: only support windows
+    const res = file.match(/\\(.*)\\index\.js/);
+    console.log(res);
+    entrys[res[1]] = {
+      import: "./" + file,
+    };
+
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        filename: res[1] + ".html",
+        chunks: [res[1]],
+      })
+    );
+  });
+
+  config.entry = entrys;
+  console.log(entrys);
+  console.log(htmlWebpackPlugins);
+  config.plugins = htmlWebpackPlugins.concat(config.plugins);
+
+  return config;
 };
