8172dbc6bab11ecb7cf27779918517e880aedc8a
heappynd <heappynd@163.com>
2023-12-06 10:18:45 +0800

    fix: 1206

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const glob = require("glob");
+const TerserPlugin = require("terser-webpack-plugin");
 
 /** @type { import("webpack").Configuration } */
 const config = {
@@ -15,6 +16,17 @@ const config = {
   devtool: false,
   module: {},
   plugins: [new HtmlWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 };
 
 module.exports = () => {
