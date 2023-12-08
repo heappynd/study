f0b1c7329acb112672f01904d95ea6476a1a5b3a
heappynd <heappynd@163.com>
2023-12-08 18:10:22 +0800

    fix: 1208

---
+++ eslint-demo/src/index.js
@@ -0,0 +1,17 @@
+let a = "1";
+// ?
+MY_CUSTOM_GLOBA.x = 1;
+
+[].includes(1);
+
+Promise;
+
+/**
+ * 
+ * @param {*} n1 
+ * @param {*} n2 
+ * @returns 
+ */
+function add(n1, n2) {
+  return n1 + n2;
+}
