ac3031b1cc4ab9627c9a6a90d3701bc79fa8468b
heappynd <heappynd@163.com>
2023-12-11 23:19:00 +0800

    fix: 1211

--- cli-demo/cli-test/bin/cli-test.js
+++ cli-demo/cli-test/bin/cli-test.js
@@ -1,35 +1,66 @@
 #!/usr/bin/env node
 
-const lib = require("cli-test-lib");
-const argv = require("process").argv;
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const dedent = require("dedent");
 
-const command = argv[2];
+// cli-test --help   --->   ['--help']
+const arg = hideBin(process.argv);
+const cli = yargs(arg);
+cli
+  .usage("Usage: cli-test <command> [options]")
+  .demandCommand(1, "最少输入的命令个数")
+  .alias("h", "help")
+  .alias("v", "version")
+  // .wrap(100)
+  .wrap(cli.terminalWidth())
+  .epilogue(
+    dedent`  结尾
+加上一句话`
+  )
+  .options({
+    debug: {
+      type: "boolean",
+      describe: "Bootstrap debug mode",
+      alias: "d",
+    },
+  })
+  .option("registry", {
+    type: "string",
+    // hidden: true,
+    describe: "Define global registry",
+    alias: "r",
+  })
+  .group(["debug"], "Dev Options:")
+  .group(["registry"], "Extra Options:")
+  .command(
+    "init [name]",
+    "Do init a project",
+    (yargs) => {
+      yargs.option("name", {
+        type: "string",
+        describe: "Name of a project",
+        alias: "n",
+      });
+    },
+    (argv) => {
+      console.log("argv", argv);
+    }
+  )
+  .command({
+    command: "list",
+    aliases: ["ls", "la", "ll"],
+    describe: "List local packages",
+    builder(yargs) {},
+    handler(argv) {
+      console.log("argv", argv);
+    },
+  })
+  .recommendCommands()
+  .fail((err, msg) => {
+    console.log("err", err);
+    console.log("msg", msg);
+  })
+  .strict().argv;
 
-console.log("command", command);
-
-// console.log(argv);
-
-const options = argv.slice(3);
-
-console.log("options", options);
-
-let [option, param] = options;
-
-option = option.replace("--", "");
-
-if (lib[command]) {
-  lib[command]({ option, param });
-} else {
-  console.log("请输入命令");
-}
-
-// cli-test init --name vue-test
-/**
- * [
-  'C:\\Program Files\\nodejs\\node.exe',
-  'C:\\Program Files\\nodejs\\node_modules\\cli-test\\bin\\cli-test.js',
-  'init',
-  '--name',
-  'vue-test'
-   ]
- */
+// cli-test init -d -r npm -n vue-test
