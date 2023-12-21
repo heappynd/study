76681af850341ea5deb33faa0e429efba7aee8cd
heappynd <heappynd@163.com>
2023-12-21 23:15:22 +0800

    fix: 1221

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+const items = ref([1, 2, 3])
+
+const add = () => {
+  items.value.push(items.value.length + 1)
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <button @click="add">add</button>
+    <TransitionGroup name="list" tag="ul">
+      <li v-for="item in items" :key="item">
+        {{ item }}
+      </li>
+    </TransitionGroup>
+  </div>
 </template>
+
+<style>
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+</style>
