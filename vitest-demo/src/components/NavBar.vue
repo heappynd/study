88e9e3118f924580d0deeae9bbaa3b4a8ee2702b
heappynd <heappynd@163.com>
2024-01-31 18:22:11 +0800

    fix: 0131

---
+++ vitest-demo/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <a id="profile" href="/profile">My Profile</a>
+    <a v-if="admin" id="admin" href="/admin">Admin</a>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      admin: false,
+    };
+  },
+});
+</script>
