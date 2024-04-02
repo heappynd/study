c66b0274199e7503a4332663876fc91072442165
heappynd <heappynd@163.com>
2024-04-02 22:41:31 +0800

    fix: 0402

--- mini2024/pages/cate/cate.js
+++ mini2024/pages/cate/cate.js
@@ -1,66 +1,53 @@
 // pages/cate/cate.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    list: [1, 3, 4],
+    obj: {
+      a: 10,
+      b: 20,
+    },
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-
-  },
+  onLoad(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
-
-  },
+  onShow() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {},
 })
--- mini2024/pages/index/index.js
+++ mini2024/pages/index/index.js
@@ -1,2 +1,70 @@
 // index.js
-Page({})
+Page({
+  data: {
+    id: 1,
+    school: '大学',
+    obj: {
+      name: 'tom',
+    },
+    isChecked: false,
+    count: 0,
+    userInfo: {
+      name: '',
+      age: 0,
+    },
+    list: [
+      { id: 1, name: 'one' },
+      { id: 2, name: 'two' },
+    ],
+    value: "12345",
+    checked: false
+  },
+
+  updateList() {
+    // this.setData({
+    //   list: [1, 2, 3, 4, 5],
+    // })
+    // this.setData({
+    // 'list[1]': 100,
+    // 'list[0].name': 'music',
+    // })
+  },
+
+  updateUserInfo() {
+    // this.setData({
+    //   'userInfo.name': 'tom',
+    //   'userInfo.age': 10,
+    // })
+    // delete this.data.userInfo.age
+  },
+
+  add() {
+    console.log(this.data.count)
+    // 1 更新数据 2 驱动试图更新
+    this.setData({
+      count: this.data.count + 1,
+    })
+    // console.log(this.data.count)
+  },
+
+  handler(event) {
+    console.log('event ....', event)
+  },
+
+  handleInput(event) {
+    console.log(event)
+    console.log(event.detail.value)
+  },
+
+  btnHandler(event) {
+    // currentTarget 事件绑定者
+    // target 事件触发者
+    console.log('btnHandler', event.currentTarget.dataset)
+    // mark获取的是事件触发者 到绑定者的所有数据
+    console.log('btnHandler', event.mark)
+  },
+
+  parentHandler() {
+    console.log('parentHandler')
+  },
+})
